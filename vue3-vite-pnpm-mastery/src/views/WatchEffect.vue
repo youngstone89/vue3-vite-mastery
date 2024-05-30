6723a64ae24060a7565d89b43c856a36e5f4ed0c
youngstone89 <youngstone89@icloud.com>
2024-05-30 23:22:34 +0900

    watch and watchEffect

---
+++ vue3-vite-pnpm-mastery/src/views/WatchEffect.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <input v-model="title" placeholder="Edit title">
+        <input v-model="suffix" placeholder="Edit suffix">
+        <p>Document title: {{ fullTitle }}</p>
+    </div>
+</template>
+
+<script>
+import { ref, watchEffect } from 'vue';
+
+export default {
+    setup() {
+        const title = ref('Default Title');
+        const suffix = ref('VueApp');
+
+        const fullTitle = ref('');
+
+        watchEffect(() => {
+            fullTitle.value = `${title.value} - ${suffix.value}`;
+            document.title = fullTitle.value;
+            console.log(`Document title set to: ${document.title}`);
+        });
+
+        return { title, suffix, fullTitle };
+    }
+}
+</script>
---
+++ vue3-vite-pnpm-mastery/src/views/WatchVue.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <p>User ID: {{ userId }}</p>
+        <p>User data: {{ userData }}</p>
+    </div>
+</template>
+
+<script>
+import { watch, ref, onMounted } from 'vue';
+
+export default {
+    setup(props) {
+        const userData = ref({});
+        const userId = ref(1);
+        setInterval(() => {
+            userId.value = userId.value + 1;
+        }, 1000);
+
+        watch(userId, (newUserId, oldUserId) => {
+            console.log(`User ID changed from ${oldUserId} to ${newUserId}`);
+        });
+
+        onMounted(() => {
+            console.log('Component is mounted');
+        });
+
+        return { userData, userId };
+    }
+}
+</script >
