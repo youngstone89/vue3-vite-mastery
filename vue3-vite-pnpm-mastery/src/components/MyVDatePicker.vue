638251563053fc132f3789d76b12c948fd9832bc
youngstone89 <youngstone89@icloud.com>
2024-06-02 21:03:01 +0900

    css custom testing

--- vue3-vite-pnpm-mastery/src/components/MyVDatePicker.vue
+++ vue3-vite-pnpm-mastery/src/components/MyVDatePicker.vue
@@ -1,20 +1,81 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+
+const mode = ref('date');
+const range = ref({
+    start: new Date(2024, 5, 24),
+    end: new Date(2024, 6, 1)
+});
+const attributes = ref([
+    {
+        key: 'selected-range',
+        highlight: {
+            backgroundColor: '#42b983', // Custom color for selected range
+        },
+        dates: {
+            start: range.value.start,
+            end: range.value.end
+        }
+    },
+    // Existing attributes...
+]);
+watch(range.value, (oldV, newV) => {
+    console.log(newV.start);
+    console.log(newV.end);
+});
+</script>
 <template>
     <div class="date-picker-container">
-        <v-date-picker v-model="selectedDate" locale="kr" />
+        <v-date-picker v-model.range=range :mode="mode" :columns="2" :attribute="attributes" />
     </div>
 </template>
-
-<script setup lang="ts">
-import { ref } from 'vue';
-
-const selectedDate = ref(new Date());
-// const { vDatePicker } = useDatePicker();
-</script>
-
 <style scoped>
-.date-picker-container {
-    max-width: 400px;
-    margin: auto;
-    padding: 20px;
+:root {
+    --vc-select-hover-bg: Red;
+}
+
+:deep(.vc-base-select) {
+    & :deep(select) {
+
+        &:deep(:hover) {
+            background-color: var(--vc-select-hover-bg) !important;
+        }
+    }
+}
+
+/* Target the selected day */
+:deep(.vc-highlight) {
+    background-color: #ffeb3b !important;
+
+    & :deep(.vc-highlight-base-start) {
+        width: 0% !important;
+        border-radius: 0 !important;
+        border-right-width: 0 !important;
+        background-color: blue;
+    }
+}
+
+:deep(.vc-highlight-bg-solid) {
+    border-radius: 0 !important;
+}
+
+:deep(.vc-highlight-content-solid) {
+    color: black;
+}
+
+:deep(.vc-day-content) {
+    border-radius: 0 !important;
+}
+
+:deep(.vc-highlight-bg-outline),
+:deep(.vc-highlight-bg-none) {
+    background-color: var(--vc-highlight-outline-bg);
+    border: 2px solid;
+    border-color: var(--vc-highlight-outline-border);
+    border-radius: 0;
+}
+
+:deep(.vc-light) {
+    --vc-hover-bg: #1e3a8a;
 }
 </style>
--- vue3-vite-pnpm-mastery/src/views/VDatePickerView.vue
+++ vue3-vite-pnpm-mastery/src/views/VDatePickerView.vue
@@ -3,18 +3,11 @@
         <div class="date-picker-view">
             <MyVDatePicker />
         </div>
-        <div class="date-picker-view">
-            <MyVFlightDatePicker />
-        </div>
-        <div class="date-picker-view">
-            <VCalendarInitialPage />
-        </div>
     </main>
 </template>
 
 <script setup lang="ts">
 import MyVDatePicker from '../components/MyVDatePicker.vue';
-import MyVFlightDatePicker from '../components/MyVFlightDatePicker.vue';
 </script>
 
 <style scoped>
