eed34b4d9bad2cfb15c5cc2dd025843875e3d0dd
youngstone89 <youngstone89@icloud.com>
2025-06-15 17:41:04 +0900

    remov and draw d3

--- vue3-vite-pnpm-mastery/src/views/D3LineChartView.vue
+++ vue3-vite-pnpm-mastery/src/views/D3LineChartView.vue
@@ -1,33 +1,23 @@
 <template>
   <div>
     <h2>Vue.js and D3 Line Chart</h2>
-    <svg></svg>
+    <svg ref="chartContainer"></svg>
+    <button type="button" @click="remove">remove</button>
+    <button type="button" @click="draw">draw</button>
   </div>
 </template>
 
 <script setup lang="ts">
 import * as d3 from 'd3'
-import { onMounted } from 'vue'
-
-const data = [
-  { date: '24-Apr-07', amount: 93.24 },
-  { date: '25-Apr-07', amount: 95.35 },
-  { date: '26-Apr-07', amount: 98.84 },
-  { date: '27-Apr-07', amount: 99.92 },
-  { date: '30-Apr-07', amount: 99.8 },
-  { date: '1-May-07', amount: 99.47 },
-  { date: '2-May-07', amount: 100.39 },
-  { date: '3-May-07', amount: 100.4 },
-  { date: '4-May-07', amount: 100.81 },
-  { date: '7-May-07', amount: 103.92 },
-  { date: '8-May-07', amount: 105.06 },
-  { date: '9-May-07', amount: 106.88 },
-  { date: '10-May-07', amount: 107.34 }
-]
-onMounted(() => {
+import { onMounted, ref } from 'vue'
+const chartContainer = ref(null)
+const remove = () => {
+  d3.select(chartContainer.value).selectAll('*').remove()
+}
+const draw = () => {
   const width = 800
   const height = 500
-  const svg = d3.select('svg').attr('width', width).attr('height', height)
+  const svg = d3.select(chartContainer.value).attr('width', width).attr('height', height)
   const g = svg.append('g')
   const parseTime = d3.timeParse('%d-%b-%y')
 
@@ -77,6 +67,24 @@ onMounted(() => {
     .attr('stroke', 'steelblue')
     .attr('stroke-width', 1.5)
     .attr('d', line)
+}
+const data = [
+  { date: '24-Apr-07', amount: 93.24 },
+  { date: '25-Apr-07', amount: 95.35 },
+  { date: '26-Apr-07', amount: 98.84 },
+  { date: '27-Apr-07', amount: 99.92 },
+  { date: '30-Apr-07', amount: 99.8 },
+  { date: '1-May-07', amount: 99.47 },
+  { date: '2-May-07', amount: 100.39 },
+  { date: '3-May-07', amount: 100.4 },
+  { date: '4-May-07', amount: 100.81 },
+  { date: '7-May-07', amount: 103.92 },
+  { date: '8-May-07', amount: 105.06 },
+  { date: '9-May-07', amount: 106.88 },
+  { date: '10-May-07', amount: 107.34 }
+]
+onMounted(() => {
+  draw()
 })
 </script>
 
